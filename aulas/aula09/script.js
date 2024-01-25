6233f6b7da4e83ce5076f164298b931ccb14cf86
luisgrbenvindo@gmail.com <luisgrbenvindo@gmail.com>
2024-01-24 22:19:59 -0300

    JavaScript conta bancaria

--- aulas/aula09/script.js
+++ aulas/aula09/script.js
@@ -1,32 +1,75 @@
 class Conta {
-    constructor(numero, saldo){
+    constructor(numero, saldo = 0){
         this.numero = numero;
         this.saldo = saldo;
     }
 
-    sacarValor(){
+    Sacar(valor){
 
+        if(this.saldo >= valor && valor > 0){
+            this.saldo - valor;
+            return true; //Saque realizado com sucesso
+        } else {
+            return false; // saldo Insuficiente ou Valor Inválido
+        }
     }
 
-    depositar(){
-
+    depositar(valor){
+        if(valor > 0){
+            valor + this.saldo;
+            return true; //Deposito realizado com sucesso 
+        } else{
+            return false; //Valor Incorreto 
+        }
+    }
+    toString(){
+        return `Numero: ${this.numero} - Saldo: ${this.saldo}`
     }
 }
 
-console.log("funciona")
+
 
 class ContaCorrente extends Conta{
-    constructor(numero,saldo,limiteChequeEspecial){
+    constructor(numero,saldo,limiteChequeEspecial = 200){
         super(numero, saldo);
+        this.limiteChequeEspecial = limiteChequeEspecial
+    }
+
+    sacar(valor){
+
+        let limiteSaque = this.saldo + this.limiteChequeEspecial;       
+          if(valor > 0 && limiteSaque >= valor){
+               this.saldo -= valor;
+                  return true // Saque realizado com sucesso
+              } else{
+                    return false // Saldo insuficiente ou valor inválido
+              }
+    }
 }
-}
+
 
 class ContaPoupanca extends Conta{
-    constructor(numero,saldo,taxaRendimento){
+    constructor(numero,saldo,taxaRendimento =0.02){
         super(numero,saldo);
+        this.taxaRendimento = taxaRendimento;
+
     }
 
-    Sacar(){
+    aplicarRendimento(valor){
+        this.saldo += this.saldo * this.taxaRendimento
 
     }
 }
+
+let contaCorr = new ContaCorrente(1122,100);
+let contaPoup= new ContaPoupanca(1129,100);
+
+console.log(contaCorr.toString());
+console.log(contaPoup.toString());
+
+console.log("Apliquei rendimento a conta poupança");
+
+  contaPoup.aplicarRendimento()
+
+  console.log(contaCorr.toString());
+  console.log(contaPoup.toString());

1a7dd722ec6931d7470fe8ed9567cc269e86db90
luisgrbenvindo@gmail.com <luisgrbenvindo@gmail.com>
2024-02-02 21:20:33 -0300

    adicionando funçao no exercicio

--- aulas/exercicios/script.js
+++ aulas/exercicios/script.js
@@ -1,6 +1,12 @@
+//modifique a classe pessoa, adicionando a ela o parametro "anoNascimento"
+//crie uma função que informa se a pessoa é adulta, deve receber um ano como parametro 
+//e retornar verdadeiro ou falso
+
+
 class Pessoa {
-    constructor(nome) {
+    constructor(nome, anoNascimento) {
         this.nome = nome;
+        this.anoNascimento = anoNascimento;
     }
 
     seApresentar(pessoa) {
@@ -16,12 +22,27 @@ class Pessoa {
 
     }
 
+    ehAdulta(ano) {
+        
+        let adulto =   ano - this.anoNascimento >= 18;
+       let menorDeIdade =  ano - this.anoNascimento  >= 0 ;
+        if (adulto) {
+            return true;
+        } else if (menorDeIdade) {
+            return false;
+        }
+
+        alert( "Pessoa não era nascida" ) ;
+    }
 
 }
 
-const pessoaA = new Pessoa("Luis");
-const pessoaB = new Pessoa("Jorge");
+const pessoaA = new Pessoa("Luis", 2001);
+const pessoaB = new Pessoa("Jorge", 2015);
+
 
 pessoaA.seApresentar(pessoaB);
+
+console.log(pessoaA.ehAdulta(2001))
 
 

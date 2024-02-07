ade5cc8b5c4810681efa3d7239af2bcda0e22639
luisgrbenvindo@gmail.com <luisgrbenvindo@gmail.com>
2024-02-06 21:34:36 -0300

    Manipulação do dom Criada js

--- aulas/projetoContaBancaria/script.js
+++ aulas/projetoContaBancaria/script.js
@@ -54,11 +54,11 @@ class ContaCorrente extends ContaBancaria {
 
         const limiteEspecial = this.saldo + this.limiteCheque;
 
-        if(valorSaque <= limiteEspecial){
-        return  super.sacar(valorSaque);
+        if (valorSaque <= limiteEspecial) {
+            return super.sacar(valorSaque);
+        }
+        return false
     }
-     return false
-    }   
 }
 
 class ContaPoupanca extends ContaBancaria {
@@ -76,14 +76,55 @@ class ContaPoupanca extends ContaBancaria {
 let contas = [];
 let clientes = [];
 
-function cadastrarCliente(){
+function cadastrarCliente() {
     // pegar dados de tela;
     const nome = document.getElementById("nomeCliente").value;
     const cpf = document.getElementById("cpfCliente").value;
     //instanciar novo cliente;
-    const cliente  =  new Cliente(nome, cpf);
+    let cliente = new Cliente(nome, cpf);
+
     //add o cliente a lista de clientes;
     clientes.push(cliente);
-} 
 
+    atualizarSeletorClientes();
+}
+
+function atualizarSeletorClientes() {
+    const seletorClientes = document.getElementById("cliente");
+
+    seletorClientes.innerHTML = "";
+    clientes.forEach(cliente => {
+        const option = document.createElement("option")
+        option.value = cliente.cpf;
+        option.textContent = cliente.nome;
+        seletorClientes.appendChild(option);
+    })
+}
+
+function cadastrarConta() {
+
+    //pegar os dados da tela 
+    const numero = parseInt(document.getElementById("numero").value);
+    const saldo = parseFloat(document.getElementById("saldo").value);
+    const tipoConta = parseFloat(document.getElementById("tipoConta").value);
+    //identificar o cliente selecionado na lista de clientes
+    const clienteSelecionado = document.getElementById("cliente").value;
+    const cliente = clientes.find(c => c.cpf === clienteSelecionado);
+    //instanciar uma nova conta,a partir do tipo de conta selecionada
+
+
+    let conta;
+    switch (tipoConta) {
+        case "ContaCorrente":
+            conta = new ContaCorrente(cliente, numero, saldo, 100);
+            break;
+        case "ContaPoupanca":
+            conta = new ContaPoupanca(cliente, numero, saldo, 0.01);
+            break;
+        default:
+            alert("Tipo selecionado  invalido");
+            break
+    }
+    contas.push(conta)
+}
 
